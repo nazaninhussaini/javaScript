614462d2e7c81bc089390bb088ff053eb23c30e6
nazaninhussaini <banonazaninhussaini@gmail.com>
2025-12-10 12:48:49 +0430

    chenges

--- js-code/js-code/six.js
+++ js-code/js-code/six.js
@@ -1,16 +1,20 @@
+//Array with method reduc
 const numbers = [ 1,2,3,4,5,6,7,8,9,10];
 const result= numbers.reduce((result,number)=>{
     return result+number
 })
+//Array in method reduce example with multiplier
 const zarb = [ 2,4,6,8,10,12,13,4];
 const result_1 = zarb.reduce((result,zarb)=>{
     return result*zarb
 })
+//Array with method reduce example with string
 const myFriend = ["Masoda","rayhan","Masoda","mubareka","Masoda","nastarn","rayhan","mubareka","Masoda"];
 const friend = myFriend.reduce((first,friend)=>{
     first[friend] = (first[friend] ||0) + 1;
     return first; 
 } ,{})
+//Array with method join example with string
 const jomla = ["Hi","Iam","Nazanin","Iam","student","in","sharifi","programming"];
 const natija = jomla.join(" ");
 const name = ["Nazanin","Ali Behzad","Ahamd Farid","Mohammad"];
@@ -18,11 +22,12 @@ name.shift();
 name.unshift("nazi");
 name.pop();
 name.push("mozghan");
-
+//Array with method some example with string
 const some_every = name.some((names)=>{
     return names.includes("a")
 
 })
+//object with string proprty : to lower case 
 const students = [
     {
         id:1,
