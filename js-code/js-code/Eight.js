ebb53a5c6c52e61e5ac9a49c7379767b918ac138
nazaninhussaini <banonazaninhussaini@gmail.com>
2025-12-11 22:22:57 +0430

    add fails

---
+++ js-code/js-code/Eight.js
@@ -0,0 +1,28 @@
+const number = "23";
+const result = Number(number);
+
+const num = 99.99;
+const result_1 = parseInt(num);
+
+const float_num = 88.9;
+const result_2 = parseFloat(float_num);
+
+const plas_num = -44;
++plas_num;
+
+const math_num = 76.7
+const result_3 = Math.ceil(math_num);
+isNaN(math_num);
+
+const math = "";
+const result_4 = Math.random()*100;
+
+const num_1 = 0;
+ function sum (num_1,num_2){
+    if(isNaN (num_1)){
+        num_1 = 1
+    }
+    return num_1*num_2
+}
+sum(2,6);
+
