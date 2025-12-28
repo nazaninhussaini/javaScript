e79954731b434c5e9830bea9336db901d056ccf6
nazaninhussaini <banonazaninhussaini@gmail.com>
2025-12-28 12:52:15 +0430

    add file

---
+++ js-and-html/js-project/tree.js
@@ -0,0 +1,11 @@
+const div = document.querySelector("div");
+const para = document.querySelector("p");
+//div.classList.add("naz");
+//para.classList.add("p");
+div.className ="naz"
+div.className = `${div.className} outline`;
+//div.classlist.remove("line");
+para.setAttribute("class" , "p d");
+//para.setAttribute("class","d");
+div.style.backgroundColor = "gray";
+//div.style.outline = "4px solid oraneg"
