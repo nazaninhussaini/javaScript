3acaa7281881d12e863c196d374f93da1e3c4022
nazaninhussaini <banonazaninhussaini@gmail.com>
2025-12-31 21:37:40 +0430

    add file and codes

---
+++ js-and-html/js-project/four.js
@@ -0,0 +1,13 @@
+const div = document.querySelectorAll("div");
+const firstDiv = div[0];
+const newDiv = document.createElement("div");
+newDiv.classList.add("newBox");
+newDiv.style.height = "250px"
+newDiv.style.width = "250px"
+newDiv.style.border = "2px solid black"
+newDiv.style.borderRadius = "8px"
+newDiv.style.backgroundColor = "yellow"
+
+firstDiv.appendChild(newDiv);
+
+firstDiv.lastElementChild.before(newDiv);
