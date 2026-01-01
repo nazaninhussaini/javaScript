0ecec47c00570ab8fe39e65a4302f0f03a73fa7e
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-01-01 22:21:56 +0430

    add code

--- js-and-html/js-project/four.js
+++ js-and-html/js-project/four.js
@@ -7,9 +7,7 @@ newDiv.style.width = "250px";
 newDiv.style.border = "2px solid black";
 newDiv.style.borderRadius = "8px";
 newDiv.style.backgroundColor = "yellow";
-
 firstDiv.appendChild(newDiv);
-
 firstDiv.firstElementChild.after(newDiv);
 //div.forEach((div,index)=>{
  //   if(index === 2 || index === 3){
