34050817d2f20f2d1ad444dc327d958f0675c2cb
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-01-01 13:11:55 +0430

    code add

--- js-and-html/js-project/four.js
+++ js-and-html/js-project/four.js
@@ -2,12 +2,17 @@ const div = document.querySelectorAll("div");
 const firstDiv = div[0];
 const newDiv = document.createElement("div");
 newDiv.classList.add("newBox");
-newDiv.style.height = "250px"
-newDiv.style.width = "250px"
-newDiv.style.border = "2px solid black"
-newDiv.style.borderRadius = "8px"
-newDiv.style.backgroundColor = "yellow"
+newDiv.style.height = "250px";
+newDiv.style.width = "250px";
+newDiv.style.border = "2px solid black";
+newDiv.style.borderRadius = "8px";
+newDiv.style.backgroundColor = "yellow";
 
 firstDiv.appendChild(newDiv);
 
-firstDiv.lastElementChild.before(newDiv);
+firstDiv.firstElementChild.after(newDiv);
+//div.forEach((div,index)=>{
+ //   if(index === 2 || index === 3){
+   //     div.remove()
+    //}
+//})
